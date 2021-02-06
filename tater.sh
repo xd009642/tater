b6006f1d3aff6cf4fe4d2dd855b0e2f70dcad8b5
xd009642 <danielmckenna93@gmail.com>
2021-02-06 12:39:49 +0400

    Small updates to print less do more

--- tater.sh
+++ tater.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-
+toolchain="+nightly"
+echo "Running with toolchain $toolchain"
 export RUST_LOG="cargo_tarpaulin=info"
 mkdir results ;
 mkdir projects ;
@@ -13,8 +14,9 @@ do
     mkdir -p "$results_dir"
     echo "Cloning $name from $repo"
     echo "Saving results to $results_dir"
-    git clone "$repo" "$name" && cd "$name" ;
-    cargo tarpaulin --debug &> "$name.log"
+    git clone "$repo" "$name" &> /dev/null && cd "$name" ;
+    echo "cargo $toolchain tarpaulin --debug --color never --all-features"
+    cargo $toolchain tarpaulin --debug --color never --all-features &> "$name.log"
     mv "$name.log" "$results_dir"
     mv tarpaulin-run* "$results_dir/tarpaulin-run.json"
     cd .. 

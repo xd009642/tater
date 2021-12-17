74e543de8a9142d46f05cb84a6f5ce7c18d13ade
xd009642 <danielmckenna93@gmail.com>
2021-12-17 19:00:52 +0000

    Add in parsing of tarpaulin action

--- src/ci/github.rs
+++ src/ci/github.rs
@@ -6,6 +6,7 @@ use std::fs;
 use std::io;
 use std::path::Path;
 use std::process::{Child, Command, Stdio};
+use tracing::warn;
 
 /// The overall github actions workflow, look [here](https://docs.github.com/en/actions/learn-github-actions/workflow-syntax-for-github-actions) for
 /// docs
@@ -79,12 +80,57 @@ fn read_workflow(workflow: &Path, cmd: &mut Command) -> io::Result<Child> {
         .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.to_string()))?;
 
     for (name, job) in &workflow.jobs {
-        for step in &job.steps {
-            if step.uses.starts_with("actions-rs/tarpaulin") {
-                // Extract tarpaulin args and merge
-            } else if step.uses.starts_with("actions-rs/grcov") {
-                // Convert grcov args to tarpaulin
-            } else {
+        if let Some(step) = job
+            .steps
+            .iter()
+            .find(|x| x.uses.starts_with("actions-rs/tarpaulin"))
+        {
+            // Extract tarpaulin args and merge https://github.com/actions-rs/tarpaulin
+            for (arg, val) in step
+                .with
+                .iter()
+                .filter(|(_, v)| v.is_string())
+                .map(|(k, v)| (k, v.as_str().unwrap()))
+            {
+                match arg.as_str() {
+                    "run-types" => {
+                        cmd.arg("--run-types");
+                        cmd.args(val.split_whitespace());
+                    }
+                    "timeout" => {
+                        cmd.arg("--timeout");
+                        cmd.arg(val);
+                    }
+                    "out-type" => {
+                        cmd.arg("--out");
+                    }
+                    "args" | "version" => {
+                        let mut skip_next = false;
+                        for arg in val.split_whitespace() {
+                            if skip_next {
+                                skip_next = false;
+                                continue;
+                            }
+                            if arg == "--color" {
+                                skip_next = true;
+                                continue;
+                            }
+                            cmd.arg(arg);
+                        }
+                    }
+                    e => warn!("Unexpected with field: {}", e),
+                }
+            }
+            return cmd.spawn();
+        } else if let Some(step) = job
+            .steps
+            .iter()
+            .find(|x| x.uses.starts_with("actions-rs/cargo"))
+        {
+            // Convert grcov args to tarpaulin https://github.com/actions-rs/grcov
+            if step.with.get("command").and_then(|x| x.as_str()) == Some("test") {}
+        } else {
+            for step in &job.steps {
                 // TODO detect kcov, cargo-llvm-cov, llvm coverage, or last attempt cargo test
                 // calls
             }
